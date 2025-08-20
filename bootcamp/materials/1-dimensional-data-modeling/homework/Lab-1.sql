8b9027c2741e14f39386e5256969f85457675bc1
Trung Huynh PB <andrew.trunghpb@gmail.com>
2025-08-21 00:36:16 +0700

    Finishing Lab 1 query....

--- bootcamp/materials/1-dimensional-data-modeling/homework/Lab-1.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/Lab-1.sql
@@ -1,5 +1,14 @@
-drop table if exists players;
+--View Player Seasons table
+select * from player_seasons;
 
+-- Create Type
+drop table if exists players;
+drop type if exists season_stats;
+create type season_stats as (season integer, gp integer, pts real, reb real, ast real);
+drop type if exists scoring_class;
+create type scoring_class as enum('star','good','average','bad');
+
+-- Create Players table
 create table players (
 	player_name TEXT,
 	height TEXT,
@@ -15,49 +24,52 @@ create table players (
 	PRIMARY KEY(player_name, current_season)
 );
 
+-- Minimum of player_seasons 
+select min(season) from player_seasons --1996
 
-WITH last_season AS (
-    SELECT * FROM players
-    WHERE current_season = 1996
-
-), this_season AS (
-     SELECT * FROM player_seasons
-    WHERE season = 1997
-)
-INSERT INTO players 
-SELECT
-        COALESCE(ls.player_name, ts.player_name) as player_name,
+-- Insert table script for each year
+insert into players
+with yesterday as 
+	(select * from players where current_season= 2000)
+, today as 
+	(select * from player_seasons where season= 2001)
+select COALESCE(ls.player_name, ts.player_name) as player_name,
         COALESCE(ls.height, ts.height) as height,
         COALESCE(ls.college, ts.college) as college,
         COALESCE(ls.country, ts.country) as country,
         COALESCE(ls.draft_year, ts.draft_year) as draft_year,
         COALESCE(ls.draft_round, ts.draft_round) as draft_round,
-        COALESCE(ls.draft_number, ts.draft_number)
-            as draft_number,
-        COALESCE(ls.seasons,
-            ARRAY[]::season_stats[]
-            ) || CASE WHEN ts.season IS NOT NULL THEN
-                ARRAY[ROW(
-                ts.season,
-                ts.pts,
-                ts.ast,
-                ts.reb, ts.weight)::season_stats]
-                ELSE ARRAY[]::season_stats[] END
-            as seasons,
+        COALESCE(ls.draft_number, ts.draft_number) as draft_number,
+        case when ls.season_stats is null then ARRAY[row(ts.season,ts.gp,ts.pts,ts.reb,ts.ast)::season_stats]
+        	when ts.season is not null then ls.season_stats || ARRAY[row(ts.season,ts.gp,ts.pts,ts.reb,ts.ast)::season_stats]
+        else ls.season_stats
+        	end as season_stats,
          CASE
              WHEN ts.season IS NOT NULL THEN
                  (CASE WHEN ts.pts > 20 THEN 'star'
                     WHEN ts.pts > 15 THEN 'good'
                     WHEN ts.pts > 10 THEN 'average'
                     ELSE 'bad' END)::scoring_class
-             ELSE ls.scoring_class
-         END as scoring_class,
-         case
+             ELSE ls.scoring_class end as scoring_class,
+             case
          	when ts.season is not null then 0
          else ls.years_since_last_season + 1
          end as years_since_last_season,
-         ts.season IS NOT NULL as is_active,
-         1997 AS current_season
-    FROM last_season ls
-    FULL OUTER JOIN this_season ts
-    ON ls.player_name = ts.player_name
+       	coalesce(ts.season, ls.current_season + 1) as current_season
+from today ts full outer join yesterday ls on ts.player_name = ls.player_name
+
+--Query to test results
+
+   --First season points vs Latest season points
+select player_name, (season_stats[1]::season_stats).pts as first_season_pts ,
+	(season_stats[cardinality(season_stats)]::season_stats).pts as latest_season_pts 
+from players 
+where current_season = 2001 
+
+  --Performance query = Latest season pts / first season pts
+select player_name, (season_stats[cardinality(season_stats)]::season_stats).pts/ 
+	case when (season_stats[1]::season_stats).pts = 0 then 1 else (season_stats[1]::season_stats).pts end as performance
+from players 
+where current_season = 2001 
+order by 2 desc
+
