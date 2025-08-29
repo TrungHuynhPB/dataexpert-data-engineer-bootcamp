0a7138adab8eddb3b6882b2d60d362e727cbda49
Trung Huynh PB <andrew.trunghpb@gmail.com>
2025-08-29 23:49:12 +0700

    updating lab2 dáy

--- bootcamp/materials/2-fact-data-modeling/homework/lab2-day1.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/lab2-day1.sql
@@ -1,2 +1,8 @@
 
---Hello World
+with deduped as (
+	select g.game_date_est,gd.*, row_number() over (partition by gd.game_id, team_id, player_id order by g.game_date_est) as row_num
+	from game_details gd
+	join games g on gd.game_id = g.game_id
+)
+select * from deduped where row_num=1
+
