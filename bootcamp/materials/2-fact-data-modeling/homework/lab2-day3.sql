418c8d9c11cf455baa4c715f1d5847cd4d1f7b5c
trunghpb <andrew.trunghpb@gmail.com>
2025-09-03 23:53:14 +0700

    lab2 day 3 cont

--- bootcamp/materials/2-fact-data-modeling/homework/lab2-day3.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/lab2-day3.sql
@@ -4,4 +4,22 @@ create table array_metrics (
 	metric_name TEXT,
 	metric_array real[],
 	primary key (user_id, month_start, metric_name)
-);a
+);
+
+with daily_aggregate as (
+	select user_id, count(1) as num_site_hits
+	from events 
+	where date(:event_time) = date(:'2023-01-01')
+	and user_id is not null
+	group by user_id
+),
+yesterday_array as 
+(
+select * from array_metrics
+where month_start = DATE(:'2023-01-01')
+)
+select coalesce(da.user_id, ya.user_id) as user_id,
+	coalesce(ya.month_start, date_trunc('month',da.date)) as month_start
+	'site_hits' as metric_name,
+	
+from daily_aggregate da full outer join yesterday_array ya on da.user_i = ya.user_id
