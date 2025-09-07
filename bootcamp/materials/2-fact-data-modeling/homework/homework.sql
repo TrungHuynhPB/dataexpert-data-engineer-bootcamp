aedab8cfd2ef991b74572d0fa5d1fe16f31c2550
trunghpb <andrew.trunghpb@gmail.com>
2025-09-08 00:42:31 +0700

    Update homework for Lab 2 - create game_detail devices accumulated querys

--- bootcamp/materials/2-fact-data-modeling/homework/homework.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/homework.sql
@@ -8,53 +8,49 @@ with deduped as (
 )
 select * from deduped where row_num=1
 
-select *
-from game_details
-
 /*
  - A DDL for an `user_devices_cumulated` table that has:
   - a `device_activity_datelist` which tracks a users active days by `browser_type`
   - data type here should look similar to `MAP<STRING, ARRAY[DATE]>`
     - or you could have `browser_type` as a column with multiple rows for each user (either way works, just be consistent!)
  */
-drop table if exists users_cumulated;
-create table users_cumulated ( 
-	user_id TEXT,
-	device_id real,
-	dates_active DATE[], -- list of dates in the past where the user was active
-	date DATE, --the current date for the user
-	primary key (user_id,device_id,date)
+--Way 1
+drop table if exists user_devices_cumulated;
+CREATE TABLE user_devices_cumulated (
+    user_id TEXT,
+    device_activity JSONB,
+    snap_dt TIMESTAMP,
+    PRIMARY KEY (user_id, snap_dt)
 );
 
-drop type if exists device_activity_datelist;
-create type device_activity_datelist as
-(device_id real, browser_type text, active_date date);
-
-unnest(ARRAY[
-	ROW(device_id, browser_type, active_date)::device_activity_datelist
-]) as records
-   
-
-insert into users_cumulated
-with yesterday as ( 
-select * from users_cumulated where date = DATE('2023-01-02')
+insert into user_devices_cumulated
+WITH event_devices AS (
+    SELECT 
+        COALESCE(e.user_id, '0') AS user_id, 
+        COALESCE(e.device_id, '0') as device_id, 
+        d.browser_type,
+        CAST(e.event_time AS DATE) AS event_date 
+    FROM events e 
+    LEFT JOIN devices d ON COALESCE(e.device_id, '0') = COALESCE(d.device_id, '0')
+    GROUP BY COALESCE(e.user_id, '0'), COALESCE(e.device_id, '0'), d.browser_type, e.event_time
 ),
-today as (
-select cast(user_id as text) as user_id, DATE(cast(event_time as timestamp)) as date_active
-from events
-where DATE(cast(event_time as timestamp)) = DATE('2023-01-03')
-and user_id is not null
-group by user_id, DATE(cast(event_time as timestamp)) 
+agg_browser AS (
+    SELECT 
+        user_id,
+        device_id,
+        browser_type,
+        ARRAY_AGG(DISTINCT event_date ORDER BY event_date) AS active_dates
+    FROM event_devices
+    GROUP BY user_id, device_id, browser_type
 )
-select coalesce(t.user_id, y.user_id) as user,
-case when y.dates_active is null then array[t.date_active]
-	when t.date_active is null then y.dates_active
-end as dates_active,
-coalesce(t.date_active, y.date + interval '1 day') as date
-from today t 
-full outer join yesterday y
-on t.user_id = y.user_id;
+SELECT 
+    user_id,
+    jsonb_object_agg(COALESCE(browser_type, 'unknown'), to_jsonb(active_dates)) AS device_activity,
+    CURRENT_TIMESTAMP  AS snap_dt
+FROM agg_browser
+GROUP BY user_id;
 
+select * from user_devices_cumulated;
 --- A cumulative query to generate `device_activity_datelist` from `events`
 
 --- A `datelist_int` generation query. Convert the `device_activity_datelist` column into a `datelist_int` column 
