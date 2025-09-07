5613f42fa82778172494619d69d172ec56490ae7
trunghpb <andrew.trunghpb@gmail.com>
2025-09-07 22:35:08 +0700

    Update homework for Lab 2 - dedupping game_details table

--- bootcamp/materials/2-fact-data-modeling/homework/homework.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/homework.sql
@@ -4,8 +4,12 @@ with deduped as (
 	select g.game_date_est,g.season, g.home_team_id,gd.*, row_number() over (partition by gd.game_id, team_id, player_id order by g.game_date_est) as row_num
 	from game_details gd
 	join games g on gd.game_id = g.game_id
+	
 )
 select * from deduped where row_num=1
+
+select *
+from game_details
 
 /*
  - A DDL for an `user_devices_cumulated` table that has:
